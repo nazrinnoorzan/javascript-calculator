c0de76dc7b5f591ec98321f5cbd7afb0286b37bb
Nazrin Noorzan <nazrinnoorzan@gmail.com>
2021-06-12 22:18:23 +0800

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [result, setResult] = useState("")
+
+  // add calculation
+  function handleResult(e) {
+    setResult(result.concat(e.target.value))
+  }
+
+  // do calculation
+  function handleCalculate() {
+    try {
+      setResult(eval(result).toString())
+    } catch (err) {
+      setResult("Error in calculation!")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div className="calculator">
+        <div className="outputScreen" id="display">
+          {result}
+        </div>
+        <div>
+          <button className="jumbo" style={{ background: "rgb(172, 57, 57)" }} onClick={() => setResult("")}>
+            AC
+          </button>
+          <button value="/" style={{ background: "rgb(102, 102, 102)" }} onClick={handleResult}>
+            &divide;
+          </button>
+          <button value="*" style={{ background: "rgb(102, 102, 102)" }} onClick={handleResult}>
+            x
+          </button>
+          <button value="7" onClick={handleResult}>
+            7
+          </button>
+          <button value="8" onClick={handleResult}>
+            8
+          </button>
+          <button value="9" onClick={handleResult}>
+            9
+          </button>
+          <button value="‑" style={{ background: "rgb(102, 102, 102)" }} onClick={handleResult}>
+            ‑
+          </button>
+          <button value="4" onClick={handleResult}>
+            4
+          </button>
+          <button value="5" onClick={handleResult}>
+            5
+          </button>
+          <button value="6" onClick={handleResult}>
+            6
+          </button>
+          <button value="+" style={{ background: "rgb(102, 102, 102)" }} onClick={handleResult}>
+            +
+          </button>
+          <button value="1" onClick={handleResult}>
+            1
+          </button>
+          <button value="2" onClick={handleResult}>
+            2
+          </button>
+          <button value="3" onClick={handleResult}>
+            3
+          </button>
+          <button className="jumbo" value="0" onClick={handleResult}>
+            0
+          </button>
+          <button id="decimal" value="." onClick={handleResult}>
+            .
+          </button>
+          <button id="equals" style={{ background: "rgb(0, 68, 102)", position: "absolute", height: 130, bottom: 5 }} onClick={handleCalculate}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
